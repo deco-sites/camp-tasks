19cf385913ffeccc927b18a4ef22021a5ee38797
Clayton Severo <clayton.severo@rankme.com.br>
2024-03-19 18:00:36 -0300

    Desenvolvimento cupons

--- sections/Cupom.tsx
+++ sections/Cupom.tsx
@@ -1,15 +1,24 @@
-export interface CupomProps {
-  title: string;
-  code: string;
-  description: string;
+export interface CardProps {
+   code: string;
+   description?: string;
 }
 
-export default function Cupom({ title, code, description }: CupomProps) {
-  return (
-    <div id="cupom">
-      <h2>{title}</h2>
-      <p>{code}</p>
-      <p>{description}</p>
-    </div>
-  );
+export interface CupomProps {
+   cards: CardProps[];
+}
+
+export default function Cupom({ cards }: CupomProps) {
+   return (
+      <div class="container mx-auto">
+         <h2 class="text-8xl font-medium mb-3">Cupons</h2>
+         <ul class="flex gap-4">
+            {cards.map((card, index) => (
+               <li key={index} class="border-2 border-dashed border-emerald-300 p-2 rounded-md min-w-72">
+                  <h3 class="font-medium">{card.code}</h3>
+                  <p class="text-sm">{card.description}</p>
+               </li>
+            ))}
+         </ul>
+      </div>
+   );
 }
