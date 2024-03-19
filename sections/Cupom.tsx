544ad2a5ab197a4315e3fafeb2e7eec155613a6e
Clayton Severo <clayton.severo@rankme.com.br>
2024-03-19 14:32:26 -0300

    Desenvolvimento section cupom

---
+++ sections/Cupom.tsx
@@ -0,0 +1,15 @@
+export interface CupomProps {
+   title: string,
+   code: string,
+   description: string
+}
+
+export default function Cupom({title, code, description}:CupomProps) {
+   return (
+      <div id="cupom">
+         <h2>{title}</h2>
+         <p>{code}</p>
+         <p>{description}</p>
+      </div>
+   )
+}
