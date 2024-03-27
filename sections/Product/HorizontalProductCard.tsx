55d03e27e3cd80189da66edcc97a2b12d5e1c53e
Clayton Severo <clayton.severo@rankme.com.br>
2024-03-27 15:47:56 -0300

    Treinamento deco camp

--- sections/Product/HorizontalProductCard.tsx
+++ sections/Product/HorizontalProductCard.tsx
@@ -1,42 +1,47 @@
 import { ImageWidget } from "apps/admin/widgets.ts";
 
 export interface Product {
-   products: Props[];
+  products: Props[];
 }
 
 export interface Props {
-   image: {
-      src: ImageWidget;
-      alt?: string;
-      href?: string;
-   };
-   productName: string;
-   productDescription: string;
-   productPrice: string;
+  image: {
+    src: ImageWidget;
+    alt?: string;
+    href?: string;
+  };
+  productName: string;
+  productDescription: string;
+  productPrice: string;
 }
 
 export default function HorizontalProductCard({ products }: Product) {
-   return (
-      <div className="container">
-         <ul className="flex flex-col gap-4">
-            {products.map((product, index) => (
-               <li className="sm:flex gap-2 p-4 rounded-md shadow bg-slate-600 text-slate-200" key={index}>
-                  <div>
-                     <img
-                        src={product.image.src}
-                        alt={product.image.alt}
-                        className="rounded w-full"
-                     />
-                  </div>
-                  <div className="flex flex-col gap-2">
-                     <h2 className="text-lg font-medium">{product.productName}</h2>
-                     <p className="text-sm font-thin">{product.productDescription}</p>
-                     <p className="text-xl font-bold">{product.productPrice && `R$${product.productPrice}`}</p>
-                     <button className="btn mt-auto">Comprar</button>
-                  </div>
-               </li>
-            ))}
-         </ul>
-      </div>
-   );
+  return (
+    <div className="container">
+      <ul className="flex flex-col gap-4">
+        {products.map((product, index) => (
+          <li
+            className="sm:flex gap-2 p-4 rounded-md shadow bg-slate-600 text-slate-200"
+            key={index}
+          >
+            <div>
+              <img
+                src={product.image.src}
+                alt={product.image.alt}
+                className="rounded w-full"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h2 className="text-lg font-medium">{product.productName}</h2>
+              <p className="text-sm font-thin">{product.productDescription}</p>
+              <p className="text-xl font-bold">
+                {product.productPrice && `R$${product.productPrice}`}
+              </p>
+              <button className="btn mt-auto">Comprar</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- sections/Weather.tsx
+++ sections/Weather.tsx
@@ -1,19 +1,19 @@
 import { Temperature } from "apps/weather/loaders/temperature.ts";
 
 export interface Props {
-   temperature: Temperature | null;
-   text?: string;
+  temperature: Temperature | null;
+  text?: string;
 }
 
 export default function Weather({ temperature, text }: Props) {
-   return (
-      <div className="container mx-auto">
-         <div className="flex items-center justify-center my-4 gap-10">
-            <p className="text-medium font-medium text-8xl">
-               {temperature?.celsius}°
-            </p>
-            <p>{text}</p>
-         </div>
+  return (
+    <div className="container mx-auto">
+      <div className="flex items-center justify-center my-4 gap-10">
+        <p className="text-medium font-medium text-8xl">
+          {temperature?.celsius}°
+        </p>
+        <p>{text}</p>
       </div>
-   );
+    </div>
+  );
 }
