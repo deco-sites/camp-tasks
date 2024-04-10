5fd1f3a32c2083a2e6bd48ba02ec1fb096b94cf9
Clayton Severo <clayton.severo@rankme.com.br>
2024-04-10 15:48:51 -0300

    Desenvolvimento Matcher

--- matchers/utm.ts
+++ matchers/utm.ts
@@ -1,12 +1,14 @@
 import { MatchContext } from "deco/blocks/matcher.ts";
 
 export interface Props {
-   campaign: string;
+  campaign: string;
 }
 
 export default function Utm({ campaign }: Props, ctx: MatchContext) {
-   const url = new URL(ctx.request.url);
-   const param = new URLSearchParams(url.search).get("utmCampaign")?.includes(campaign) ?? false;
+  const url = new URL(ctx.request.url);
+  const param =
+    new URLSearchParams(url.search).get("utmCampaign")?.includes(campaign) ??
+      false;
 
-   return param;
+  return param;
 }
--- sections/Cupom.tsx
+++ sections/Cupom.tsx
@@ -2,29 +2,29 @@
  * @title {{code}}
  */
 export interface CardProps {
-   code: string;
-   description?: string;
+  code: string;
+  description?: string;
 }
 
 export interface CupomProps {
-   cards: CardProps[];
+  cards: CardProps[];
 }
 
 export default function Cupom({ cards }: CupomProps) {
-   return (
-      <div class="container px-4">
-         <h2 class="text-2xl font-medium mb-3">Cupons Disponíveis</h2>
-         <ul class="flex flex-wrap gap-4">
-            {cards.map((card, index) => (
-               <li
-                  key={index}
-                  class="shadow rounded-md p-3 min-w-full sm:min-w-40"
-               >
-                  <h3 class="font-medium text-zinc-500">{card.code}</h3>
-                  <p class="text-sm text-zinc-500">{card.description}</p>
-               </li>
-            ))}
-         </ul>
-      </div>
-   );
+  return (
+    <div class="container px-4">
+      <h2 class="text-2xl font-medium mb-3">Cupons Disponíveis</h2>
+      <ul class="flex flex-wrap gap-4">
+        {cards.map((card, index) => (
+          <li
+            key={index}
+            class="shadow rounded-md p-3 min-w-full sm:min-w-40"
+          >
+            <h3 class="font-medium text-zinc-500">{card.code}</h3>
+            <p class="text-sm text-zinc-500">{card.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
